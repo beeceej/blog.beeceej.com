4b731e4a4a9b0c311dd94f540b60ce014bbf60b7
beeceej <beeceej.code@gmail.com>
2018-08-12 15:18:19 -0500

    upgrade home text

--- src/App/routes/home/Home.js
+++ src/App/routes/home/Home.js
@@ -1,14 +1,13 @@
 import React from 'react'
 import ReactMarkdown from 'react-markdown'
 import homeStyles from './Home.scss'
+import styles from '../../Style.scss'
 const about = `
-Software engineer from North Carolina, Working in Chicago for **[Label Insight](https://github.com/labelinsight)**
+Software engineer from North Carolina, Working in Chicago for **[Label Insight](https://labelinsight.com)**
 
-Lately I've been enjoying Go, Javascript, Python, Haskell, AWS and Serverless
+Lately I've been enjoying Go, Javascript, Python, Functional Programming, AWS and Serverless
 
-**[@_beeceej](https://www.twitter.com/_beeceej) | [github.com/beeceej](https://github.com/beeceej)**
-
-![asdf](https://static.beeceej.com/posts/IMG_1023-2.png)
+![asdf](https://static.beeceej.com/posts/IMG_1023-2.jpg)
 `
 
 const Home = () => (
