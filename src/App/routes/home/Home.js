412dfb962d3c48aff8c9bd2e64520d6d50ec15a0
Brian Jones <beeceej.code@gmail.com>
2018-08-26 15:52:38 -0500

    very hacky CSS fix for mobile

--- src/App/routes/home/Home.js
+++ src/App/routes/home/Home.js
@@ -1,18 +1,21 @@
 import React from 'react'
-import ReactMarkdown from 'react-markdown'
+import Markdown from 'react-markdown'
 import homeStyles from './Home.scss'
-import styles from '../../Style.scss'
+
 const about = `
 Software engineer from North Carolina, Working in Chicago for **[Label Insight](https://labelinsight.com)**
 
 Lately I've been enjoying Go, Javascript, Python, Functional Programming, AWS and Serverless
-
-![asdf](https://static.beeceej.com/posts/IMG_1023-2.jpg)
 `
-
+const imageURI = 'https://static.beeceej.com/posts/IMG_1023-2.jpg'
 const Home = () => (
-  <div className={`${homeStyles.home}`}>
-    <ReactMarkdown source={about} />
+  <div>
+    <div className={`${homeStyles.home}`}>
+      <Markdown escapeHtml source={about} />
+    </div>
+    <div className={`${homeStyles.home}`}>
+      <img src={imageURI} />
+    </div>
   </div>
 )
 export default Home
