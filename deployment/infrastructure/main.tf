a194b7bfe973cce6d0cc6074ba62b4b99eac1a9f
Brian Jones <beeceej.code@gmail.com>
2019-07-06 23:21:25 -0500

    feat: add cloudflare worker terraform

--- deployment/infrastructure/main.tf
+++ deployment/infrastructure/main.tf
@@ -4,6 +4,15 @@ module "blog-beeceej-com" {
   bucket_policy_json = "${data.aws_iam_policy_document.blog-beeceej-com.json}"
 }
 
+module "single_worker" {
+  source                  = "./modules/cloudflare_worker"
+  script_name = "${var.bucket_name}-worker"
+  enabled_route_patterns  = ["${var.bucket_name}/*"]
+  disabled_route_patterns = ["${var.bucket_name}/"]
+  account_zone            = "beeceej.com"
+  script_path             = "${path.module}/worker/index.js"
+}
+
 resource "cloudflare_record" "blog-beeceej-com_CNAME" {
   domain  = "beeceej.com"
   name    = "${var.bucket_name}"
@@ -24,3 +33,4 @@ data aws_iam_policy_document "blog-beeceej-com" {
     ]
   }
 }
+
